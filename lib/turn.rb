981b9654b26aee541526406d2223e2a7138d1798
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-27 23:40:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,18 +43,6 @@ def turn(board)
     turn(board)
 end
  
- def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
-  end 
- def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
- end
 end
  
    
