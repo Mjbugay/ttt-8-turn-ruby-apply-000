dfc6f818f5633e976ce4ca895e54b8a16f5664a0
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-27 23:27:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,8 +37,9 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  # if valid_move?(board, index) == false
-  # move(board, index, current_player(board))
-  # else  
-  #   turn(board)
+  if valid_move?(board, index) == false
+  
+  move(board, index, current_player(board))
+  else  
+    turn(board)
 end
