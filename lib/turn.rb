3b09c9097ee16dc8b5876eb4bdf446213884f834
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-27 23:28:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == false
-  
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   move(board, index, current_player(board))
   else  
     turn(board)
